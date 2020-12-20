0bd3c3cdef0145600a58c378d8dc2462338c360b
ShreyaSirohi <72790259+ShreyaSirohi@users.noreply.github.com>
2020-12-20 15:13:13 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,37 @@
+from random import choice
+
+wordlist = ['python', 'computer', 'butterfly', 'money', 'molecule', 'geometry']
+word = choice(wordlist)
+turns = len(word)*2
+guesses = []
+
+for j in word:
+    print('_')
+
+while turns >= 0:
+    correct = 0
+    ask = input("Guess the alphabet ")
+    guesses.append(ask)
+
+    for i in word:
+        if i in guesses:
+            print(i)
+            correct += 1
+
+        elif i not in guesses:
+            print("_")
+
+    if correct == len(word):
+        print(f"You Win!!! \nthe Word is '{word}'")
+        break
+    elif turns == 0:
+        print('You Lose... :(')
+
+    if ask not in word:
+        print('Wrong!', 'You have', turns, 'turns left.')
+        if turns == 0:
+            print('Wrong... no turn left')
+    else:
+        print(turns, 'turns left')
+
+    turns -= 1
